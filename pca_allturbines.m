e2f8f3bcc403a9f378c665b670ea74fc08f4fc87
veetirajaniemi <veeti.rajaniemi@gmail.com>
2025-09-11 16:59:49 +0300

    Update pca_allturbines.m
    Added 2D and 3D plots with 2 PCs and biplots

--- pca_allturbines.m
+++ pca_allturbines.m
@@ -59,6 +59,7 @@ xlabel("Principal Component")
 ylabel("Cumulative Variance Explained")
 %%
 % Let's try a 3D plot
+close all
 
 index2_end = count2
 index14_end = index2_end + count14
@@ -70,7 +71,25 @@ WT39_faulty_norm = zscore(WT39_faulty)
 WT14_faulty_PC = WT14_faulty_norm * W(:,1:pcs)
 WT39_faulty_PC = WT39_faulty_norm * W(:,1:pcs)
 
-figure 
+
+
+subplot(1,2,1)
+grid on
+hold on
+plot(PC(1:index2_end,1),PC(1:index2_end,2), 'g*')
+hold on
+plot(PC(index14_end+1:index39_end,1),PC(index14_end+1:index39_end,2), 'k*')
+hold on
+plot(PC(index2_end+1:index14_end,1),PC(index2_end+1:index14_end,2), 'c*')
+hold on
+plot(WT14_faulty_PC(:,1), WT14_faulty_PC(:,2), 'm*')
+hold on
+plot(WT39_faulty_PC(:,1), WT39_faulty_PC(:,2), 'y*')
+xlabel('PC1'), ylabel('PC2')
+legend('WT2', 'WT39', 'WT14', 'WT14 faulty', 'WT39 faulty')
+
+subplot(1,2,2)
+hold on
 grid on
 hold on
 plot3(PC(1:index2_end,1),PC(1:index2_end,2),PC(1:index2_end,3), 'g*')
@@ -83,6 +102,26 @@ plot3(WT14_faulty_PC(:,1), WT14_faulty_PC(:,2), WT14_faulty_PC(:,3), 'm*')
 hold on
 plot3(WT39_faulty_PC(:,1), WT39_faulty_PC(:,2), WT39_faulty_PC(:,3), 'y*')
 xlabel('PC1'), ylabel('PC2'), zlabel('PC3')
+axis([-10 10  -3 5  -3 3])
+view(25, 20)
+%legend('WT2', 'WT39', 'WT14', 'WT14 faulty', 'WT39 faulty')
+sgtitle("2D and 3D PCA Plots")
 
-legend('WT2', 'WT39', 'WT14', 'WT14 faulty', 'WT39 faulty')
+%%
+close all
+
+
+sgtitle('Biplot for each Turbine')
+subplot(1,3,1)
+biplot(W(:,1:2), scores=T(1:index2_end,1:2))
+title('WT2')
+subplot(1,3,2)
+scoreMatrix = [T(index2_end+1:index14_end,1:2);WT14_faulty_norm(:,1:2)]
+biplot(W(:,1:2), scores=scoreMatrix)
+title('WT14')
+subplot(1,3,3)
+scoreMatrix = [T(index14_end+1:index39_end,1:2);WT39_faulty_norm(:,1:2)]
+biplot(W(:,1:2), scores=scoreMatrix)
+title('WT39')
+
 
