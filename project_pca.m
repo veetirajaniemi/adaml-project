a759d05439c4d7589c5f7a96b2035fdc8822f037
tonska112 <toni.viljami.koskinen@gmail.com>
2025-09-11 09:46:23 +0300

    Update project_pca.m

--- project_pca.m
+++ project_pca.m
@@ -3,7 +3,8 @@ close all
 clc
 
 %Wind-turbine datasets
-WT2 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=1,NumHeaderLines=1);
-WT3 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=2,NumHeaderLines=1);
-WT14 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=3,NumHeaderLines=1);
-WT9 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=4,NumHeaderLines=1);
+path = 'data.xlsx';
+WT2 = readtable(path,Sheet=1,NumHeaderLines=1);
+WT3 = readtable(path,Sheet=2,NumHeaderLines=1);
+WT14 = readtable(path,Sheet=3,NumHeaderLines=1);
+WT9 = readtable(path,Sheet=4,NumHeaderLines=1);
