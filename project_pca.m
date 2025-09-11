0444fc45609839970087b6fe0f4004e5bdd7445d
tonska112 <toni.viljami.koskinen@gmail.com>
2025-09-11 09:37:50 +0300

    Create project_pca.m
    First commit

---
+++ project_pca.m
@@ -0,0 +1,9 @@
+clearvars
+close all
+clc
+
+%Wind-turbine datasets
+WT2 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=1,NumHeaderLines=1);
+WT3 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=2,NumHeaderLines=1);
+WT14 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=3,NumHeaderLines=1);
+WT9 = readtable('C:\Users\toniv\OneDrive\Työpöytä\ADAML\data.xlsx',Sheet=4,NumHeaderLines=1);
