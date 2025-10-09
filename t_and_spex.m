d78c6bd3e17371c7889c64acd0fab0164b068064
valtanenamanda <valtanenamanda@gmail.com>
2025-10-09 11:40:58 +0300

    Update t_and_spex.m
    faulty turbines added

--- t_and_spex.m
+++ t_and_spex.m
@@ -127,3 +127,74 @@ hold off
 
 sgtitle('PCA-based Control Charts')  % yhteinen otsikko
 
+%% faulty turbines projected
+
+% Normalization
+WT39_normalized = zscore(WT39);
+
+T2_f = funcs.t2comp(WT39_normalized, coeffs, latent, k);   
+Q_f  = funcs.qcomp(WT39_normalized,  coeffs, k);   
+figure;
+
+subplot(1,2,1)
+plot(T2_f,'-','LineWidth',1.2,'Color',plotcol);
+hold on
+yline(mu_T2,'-','Color',[0.2 0.2 0.2]);
+yline(warn_T2,'--','Color',[0.3 0.3 0.3],'LineWidth',2);
+yline(alarm_T2,'-','Color',[0.3 0.3 0.3],'LineWidth',2);
+xlabel('Sample index');
+ylabel('T^2');
+title(sprintf('T^2 Control Chart',k));
+legend({'T^2','Mean','Warning (95%C.I.)','Alarm (99.5%C.I.)'},'Location','best');
+grid on
+hold off
+
+subplot(1,2,2)
+plot(Q_f,'-','LineWidth',1.2,'Color',plotcol);
+hold on
+yline(mu_Q,'-','Color',[0.2 0.2 0.2]);
+yline(warn_Q,'--','Color',[0.3 0.3 0.3],'LineWidth',2);
+yline(alarm_Q,'-','Color',[0.3 0.3 0.3],'LineWidth',2);
+xlabel('Sample index');
+ylabel('Q (SPE)');
+title(sprintf('Q Control Chart',k));
+legend({'Q','Mean','Warning (95%C.I.)','Alarm (99.5%C.I.)'},'Location','best');
+grid on
+hold off
+
+sgtitle('PCA-based Control Charts') 
+
+% Normalization
+WT14_normalized = zscore(WT14);
+
+T2_f2 = funcs.t2comp(WT14_normalized, coeffs, latent, k);   
+Q_f2  = funcs.qcomp(WT14_normalized,  coeffs, k);   
+figure;
+
+subplot(1,2,1)
+plot(T2_f2,'-','LineWidth',1.2,'Color',plotcol);
+hold on
+yline(mu_T2,'-','Color',[0.2 0.2 0.2]);
+yline(warn_T2,'--','Color',[0.3 0.3 0.3],'LineWidth',2);
+yline(alarm_T2,'-','Color',[0.3 0.3 0.3],'LineWidth',2);
+xlabel('Sample index');
+ylabel('T^2');
+title(sprintf('T^2 Control Chart',k));
+legend({'T^2','Mean','Warning (95%C.I.)','Alarm (99.5%C.I.)'},'Location','best');
+grid on
+hold off
+
+subplot(1,2,2)
+plot(Q_f2,'-','LineWidth',1.2,'Color',plotcol);
+hold on
+yline(mu_Q,'-','Color',[0.2 0.2 0.2]);
+yline(warn_Q,'--','Color',[0.3 0.3 0.3],'LineWidth',2);
+yline(alarm_Q,'-','Color',[0.3 0.3 0.3],'LineWidth',2);
+xlabel('Sample index');
+ylabel('Q (SPE)');
+title(sprintf('Q Control Chart',k));
+legend({'Q','Mean','Warning (95%C.I.)','Alarm (99.5%C.I.)'},'Location','best');
+grid on
+hold off
+
+sgtitle('PCA-based Control Charts') 
